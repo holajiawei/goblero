e4fcbf10f88e7472c04e286ebc078c1fff9197d9
holajiawei <jiawei1227@pm.me>
2020-02-06 20:41:06 +0800

    fix badger defaultOptions

--- pkg/blero/queue.go
+++ pkg/blero/queue.go
@@ -56,9 +56,7 @@ func (q *queue) start() error {
 	}
 
 	// open db
-	badgerOpts := badger.DefaultOptions
-	badgerOpts.Dir = q.opts.DBPath
-	badgerOpts.ValueDir = q.opts.DBPath
+	badgerOpts := badger.DefaultOptions(q.opts.DBPath)
 	badgerOpts.Logger = &badgerLogger{}
 	badgerOpts.SyncWrites = true
 
